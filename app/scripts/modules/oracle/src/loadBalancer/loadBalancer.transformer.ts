6eac35ab119c0aa57e4819843d4242e6b59333b2
Deva Sagar <deva.sagar@oracle.com>
2018-09-27 18:08:02 -0400

    Oracle LB - attach LB and backend set to server group

--- app/scripts/modules/oracle/src/loadBalancer/loadBalancer.transformer.ts
+++ app/scripts/modules/oracle/src/loadBalancer/loadBalancer.transformer.ts
@@ -44,7 +44,6 @@ export class OracleLoadBalancerTransformer {
   }
 
   public convertLoadBalancerForEditing(loadBalancer: IOracleLoadBalancer): IOracleLoadBalancerUpsertCommand {
-    // const applicationName = NameUtils.parseLoadBalancerName(loadBalancer.name).application;
     if (loadBalancer.listeners) {
       Object.keys(loadBalancer.listeners).forEach(key => {
         const lis = loadBalancer.listeners[key];
@@ -130,61 +129,3 @@ export class OracleLoadBalancerTransformer {
 
 export const ORACLE_LOAD_BALANCER_TRANSFORMER = 'spinnaker.oracle.loadBalancer.transformer';
 module(ORACLE_LOAD_BALANCER_TRANSFORMER, []).service('oracleLoadBalancerTransformer', OracleLoadBalancerTransformer);
-/*
-module.exports = angular
-  .module('spinnaker.oracle.loadBalancer.transformer', [])
-  .factory('oracleLoadBalancerTransformer', function($q : IQService) {
-    function normalizeLoadBalancer(loadBalancer: IOracleLoadBalancer): IPromise<IOracleLoadBalancer> {
-      return $q.resolve(loadBalancer);
-    }
-
-    function convertLoadBalancerForEditing(loadBalancer: IOracleLoadBalancer): IOracleLoadBalancerUpsertCommand
-    {
-      // const applicationName = NameUtils.parseLoadBalancerName(loadBalancer.name).application;
-      const toEdit: IOracleLoadBalancerUpsertCommand = {
-        name: loadBalancer.name,
-        cloudProvider: loadBalancer.cloudProvider,
-        credentials: loadBalancer.account,
-        region: loadBalancer.region,
-        shape: loadBalancer.shape,
-        isPrivate: loadBalancer.isPrivate,
-        subnetIds: loadBalancer.subnetIds,
-        listeners: loadBalancer.listeners,
-        hostnames: loadBalancer.hostnames,
-        backendSets: loadBalancer.backendSets,
-        freeformTags: loadBalancer.freeformTags,
-        loadBalancerType: loadBalancer.loadBalancerType,
-        securityGroups: loadBalancer.securityGroups,
-        vpcId: loadBalancer.vpcId,
-      };
-      return toEdit;
-    }
-
-    function constructNewLoadBalancerTemplate(application: Application): IOracleLoadBalancerUpsertCommand {
-      var defaultCredentials = application.defaultCredentials.oracle || OracleProviderSettings.defaults.account,
-        defaultRegion = application.defaultRegions.oracle || OracleProviderSettings.defaults.region;
-      return {
-        name: undefined,
-        cloudProvider: 'oracle',
-        credentials: defaultCredentials,
-        region: defaultRegion,
-        shape: null,
-        isPrivate: false,
-        subnetIds: [],
-        listeners: [],
-        hostnames: [],
-        backendSets: [],
-        freeformTags: {},
-        loadBalancerType: null,
-        securityGroups: [],
-        vpcId: null
-      };
-    }
-
-    return {
-      normalizeLoadBalancer: normalizeLoadBalancer,
-      convertLoadBalancerForEditing: convertLoadBalancerForEditing,
-      constructNewLoadBalancerTemplate: constructNewLoadBalancerTemplate,
-    };
-  });
-*/
